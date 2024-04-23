b2b1b79c28f8417ca4547b4cfdefd6023c0c215e
Sulaiman <suligzus@gmail.com>
2024-04-23 15:52:53 +0200

    second commit

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+hotel_cost, plane_cost, car_rental, holiday_cost
+"""
+
+
+class Flight:
+    """ Represents a flight. 
+    Properties:
+    -  destination(str): The destination of this flight. 
+    -  cost(float): The cost of this flight. 
+    -  rental_cost(float): The cost of rent at this destination. 
+    """
+    def __init__(self, destination, cost, rental_cost):
+        self.destination = destination
+        self.cost = cost
+        self.rental_cost = rental_cost
+
+    def get_rent(self):
+        print(
+            f"It costs ${self.rental_cost} to rent a car at {self.destination}"
+        )
+
+    def __str__(self):
+        return f"{self.destination} - ${self.cost}"
+
+
+FLIGHTS = [
+    Flight("Work", 1200, 20),
+    Flight("Home", 200, 200),
+]
+""" Contains Flight information. """
+
+def get_flight(destination):
+    """_summary_
+
+    :param destination: The destination.
+    :type destination: str
+    """
+    ...
+
+
+
+for f, flight in enumerate(FLIGHTS):
+    print(f"{f}: {flight}")
+
+user_selection = int(input("Choose a place"))
+user_flight = FLIGHTS[user_selection]
+
+# Selects a destination
+user_flight.get_rent()
